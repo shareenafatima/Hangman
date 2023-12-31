44cc7ad7e34e32fc4c6e741b831e4324732d32aa
shareena fatima <shareenafatima40@gmail.com>
2024-01-01 02:21:53 +0500

    modify

--- main.rb
+++ main.rb
@@ -1,113 +1,148 @@
-require 'yaml'
-require './player'
-require './word'
-require './serialize'
+require 'fileutils'
 
-class Game 
-include Serialize
-attr_accessor :player,:word,:life,:display
+class Hangman
+  attr_accessor :word, :guessed_letters, :attempts_left
 
-def initialize
-    @word = SecretWord.new
-    @player = Player.new
-    @display = ''
-    @life = 10
-    saved_or_new
-end
-
-def saved_or_new
-puts "Enter '1' for a new game and '2' for a saved game."
-input = gets.chomp
-  if input == '1'
-    @word.select_word
-    play_game
-  else
-    old_game
-puts @display.join(' ')
-play_game
-  end
+  def initialize(word, guessed_letters = [], attempts_left = 6)
+    @word = word.downcase
+    @guessed_letters = guessed_letters.map(&:downcase)
+    @max_attempts = 6
+    @attempts_left = attempts_left
   end
 
-def display
-    if @display == ''
-        length = word.choice.strip!.length
-        @display = Array.new(length,'_')
-        puts @display.join(' ')
-        puts "\n"
-    else 
-        false
+  def display_word
+    display = ""
+    @word.chars { |letter| display += @guessed_letters.include?(letter) ? letter : '_' }
+    display
+  end
+
+  def make_guess(letter)
+    letter.downcase!
+    return if @guessed_letters.include?(letter)
+
+    @guessed_letters << letter
+    @attempts_left -= 1 unless @word.include?(letter)
+  end
+
+  def game_over?
+    @attempts_left.zero? || win?
+  end
+
+  def win?
+    @word.chars.all? { |letter| @guessed_letters.include?(letter) }
+  end
+
+  def to_s
+    "Word: #{display_word}\nAttempts left: #{@attempts_left}"
+  end
+end
+
+class HangmanGame
+  SAVED_GAMES_FOLDER = "saved_games"
+
+  def initialize
+    @dictionary = load_dictionary
+    @saved_games_folder = SAVED_GAMES_FOLDER
+    FileUtils.mkdir_p(@saved_games_folder) unless File.directory?(@saved_games_folder)
+  end
+
+  def load_dictionary
+    File.readlines("google-10000-english.txt").map(&:strip)
+  end
+
+  def select_random_word
+    @dictionary.sample
+  end
+
+  def save_game(game, filename)
+    File.open(File.join(@saved_games_folder, "#{filename}.txt"), 'w') { |file| file.puts(Marshal.dump(game)) }
+  end
+
+  def load_saved_games
+    saved_games = []
+    Dir.glob(File.join(@saved_games_folder, '*.txt')).each do |file_path|
+      saved_games << File.basename(file_path, '.txt')
     end
-end
+    saved_games
+  end
 
-def save_game
-if player.guess == 'save'
-    puts 'Enter a filname without space.'
-    filename = gets.chomp
-    to_yaml(filename)
-end
-end
+  def load_game(filename)
+    file_path = File.join(@saved_games_folder, "#{filename}.txt")
+    return nil unless File.exist?(file_path)
 
-  def match
-    return false if @player.guess == 'save'
-    if @word.choice.include?(@player.guess) 
-        word_array = @word.choice.split(/ /)
-        word_array.each_with_index do |letter,index|
-            @display[index] = letter if letter == @player.guess
-        end
-        puts "\ngreat, '#{player.guess}' is in the word.\n"
-        puts @display.join(' ')
-    else
-         miss
-    end
-end
+    Marshal.load(File.read(file_path))
+  end
 
-  def miss
-player.misses << @player.guess
-puts "\n '#{player.guess}' is not in the word."
-puts "Missess: #{player.misses.join(',')}"
-puts "\n"
-@life -= 1
-puts @display.join(' ')
-puts "\n"
-end
-
-def winner
-    if @display == @word.choice.split(/ /)
-puts "Hurry! you win"
-@life = 0
-    elsif @life.zero?
-    puts "you lose, The word was '#{@word.choice}'."
-    end
-end
-
-def replay
-    puts "Enter 'y' to play again and 'n' to exit."
-    output = get.chomp.downcase
-    if output == 'y'
-        new_game
-    elsif output == 'n'
-        puts"thanks for playing"
-        return
-    end
-end
-
-def new_game
-    new = Game.new
-    new.play_game
-end
+  def play_again?
+    puts "\nDo you want to play again? (y/n): "
+    gets.chomp.downcase == 'y'
+  end
 
   def play_game
-    display
-    until @life.zero?
-        puts "life left: #{@life}"
-@player.player_input
-save_game
-match
-winner
+    loop do
+      puts "Welcome to Hangman!"
+      puts "1. Start a new game"
+      puts "2. Load a saved game"
+      print "Choose an option (1 or 2): "
+      option = gets.chomp.to_i
+
+      if option == 1
+        word_to_guess = select_random_word
+        game = Hangman.new(word_to_guess)
+      elsif option == 2
+        saved_games = load_saved_games
+        if saved_games.empty?
+          puts "No saved games found. Starting a new game."
+          word_to_guess = select_random_word
+          game = Hangman.new(word_to_guess)
+        else
+          puts "Select a saved game:"
+          saved_games.each_with_index { |saved_game, index| puts "#{index + 1}. #{saved_game}" }
+          print "Enter the number of the saved game: "
+          selected_game_index = gets.chomp.to_i - 1
+          game = load_game(saved_games[selected_game_index])
+        end
+      else
+        puts "Invalid option. Starting a new game."
+        word_to_guess = select_random_word
+        game = Hangman.new(word_to_guess)
+      end
+
+      until game.game_over?
+        puts "\n#{game}"
+        print "Guess a letter or type 'save' to save the game: "
+        input = gets.chomp
+
+        if input.downcase == 'save'
+          print "Enter the filename to save: "
+          filename = gets.chomp
+          save_game(game, filename)
+          puts "Game saved as #{filename}."
+          next
+        end
+
+        if input =~ /^[a-zA-Z]+$/ && input.length == 1
+          if game.guessed_letters.include?(input.downcase)
+            puts "You already guessed '#{input}'. Try a different letter."
+          else
+            game.make_guess(input)
+          end
+        else
+          puts "Invalid input. Please enter a valid alphabetical letter or 'save' to save the game."
+        end
+      end
+
+      if game.win?
+        puts "\nCongratulations! You guessed the word: #{game.word}"
+      else
+        puts "\nGame over! The word was: #{game.word}"
+      end
+
+      break unless play_again?
     end
-    replay
   end
 end
 
-puts "Welcome to Hangman.\nYou got 10 attempts to guess the word.\nBest of luck!".center(100)
-Game.new
+# Example usage
+hangman_game = HangmanGame.new
+hangman_game.play_game
